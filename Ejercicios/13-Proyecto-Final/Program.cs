786d55217a351c6110ad8b7819d9a2e023e4c1aa
Gustavo Altamirano <gustavoaltamirano@unah.hn>
2020-12-14 21:01:38 -0600

    proyectoIniciando Proyecto

---
+++ Ejercicios/13-Proyecto-Final/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _13_Proyecto_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario;
+            string pass;
+            Console.WriteLine("ORDENES Y FACTURACION  --  CAFEMANIA");
+            Console.WriteLine("====================================");
+            Console.WriteLine("Ingrese su usuario: ");
+            //usuario = Console.ReadLine();
+            Console.WriteLine("Ingrese su password: ");
+            //pass = Console.ReadLine();
+        }
+    }
+}
